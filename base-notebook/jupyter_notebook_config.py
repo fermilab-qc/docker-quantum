9beed391b84281e2743b74c54d849725718d907c
Burt Holzman <burt@fnal.gov>
2018-09-28 16:25:01 -0500

    Revise display URL to always show 127.0.0.1:8888

--- base-notebook/jupyter_notebook_config.py
+++ base-notebook/jupyter_notebook_config.py
@@ -11,6 +11,7 @@ c = get_config()
 c.NotebookApp.ip = '*'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
+c.NotebookApp.custom_display_url = 'http://127.0.0.1:8888'
 
 # https://github.com/jupyter/notebook/issues/3130
 c.FileContentsManager.delete_to_trash = False
